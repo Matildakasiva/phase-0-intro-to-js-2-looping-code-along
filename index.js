793534d60a2d8adf7f20c5c3ce18bb708ce24257
Matilda <ogarimatilda@gmail.com>
2024-03-31 16:46:22 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+
+let names = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards(names , surprise){
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!`
+        
+        messages.push(message)
+    }
+    return messages
+}
+let surprise = 'surprise'
+let cards = writeCards(names, surprise)
+console.log(cards)
+
+
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
+countDown(4)
